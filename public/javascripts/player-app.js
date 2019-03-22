c8142ff2910e368749e67a31085a8e5097aebbcf
Simone Casarosa <scasarosa@enetworks.it>
2019-03-22 15:21:26 +0100

    Basic zoom in/out function for players

--- public/javascripts/player-app.js
+++ public/javascripts/player-app.js
@@ -1,4 +1,24 @@
-require(['common'], function (common) {
-    console.log('player-app.js running');
-    require(['player-listener']);
+require(["common"], function(common) {
+  console.log("player-app.js running");
+  require(["player-listener"]);
+
+  require(["jquery"], function(jquery) {
+    var $ = jquery;
+    const formatAsPercentage = x => `${Math.round(x * 100)}%`;
+    function changeScale(add) {
+      var oldMap = document.querySelector(".map");
+      var width = parseFloat(oldMap.style.width) / 100.0;
+      var newWidth = add ? width + 0.1 : width - 0.1;
+      var bothMaps = document.querySelectorAll(".map");
+      bothMaps.forEach(node => {
+        node.style.width = formatAsPercentage(newWidth);
+      });
+    }
+    $("#btn-zoom-in").click(function() {
+      changeScale(true);
+    });
+    $("#btn-zoom-out").click(function() {
+      changeScale(false);
+    });
+  });
 });
